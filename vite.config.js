2389bd03f37f213cf51ebc5f11978aecfa78bc7c
Berroome <beeromeo666@gmail.com>
2025-11-20 02:23:14 +0100

    dasda

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  base: '/tribunportofolio.github.io/', // <-- set to "/" for username.github.io, or "/repo-name/" for project site
+  plugins: [react()],
 })
